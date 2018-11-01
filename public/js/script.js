8de398059fa29a8c75f286978dbdf915c280c2d5
Donderstal <daanonderstal@hotmail.com>
2018-11-01 13:13:07 +0100

    added function retrieving API data and storing it after selection

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,23 @@
+
+function retrieveAPIData(api) {
+    let list = api.list
+    let newArray = []
+    for (i = 0; i < list.length; i++)  {
+        if (i % 2 == 0) {
+            newArray.push(list[i])
+        }
+    }
+    return newArray
+}
+
 function adamAjax() {
     $.ajax({
         method: 'GET',
         url: 'http://api.openweathermap.org/data/2.5/forecast?id=2759794&appid=d4daeba3e355348ac9050efcae9375ca',
         success: function(response) {
-            console.log(response.list[0].dt_txt)
-            document.getElementById("ajaxTest").innerHTML = response.list[0].dt_txt
+/*             console.log(response.list[0].dt_txt)
+            document.getElementById("ajaxTest").innerHTML = response.list[0].dt_txt */
+            console.log(retrieveAPIData(response))
         },
         error: function() {
             console.log(response)
